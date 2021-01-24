f0a00c51e20c32467b1d9980cc918c31899afee9
Jonathan Barone <johnito.barone@gmail.com>
2021-01-24 23:20:57 +0800

    revert url

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
 export const CALLBACK_PATH = '/lastfm/callback';
 
-// export const baseURL = 'https://api-backtube.herokuapp.com'
+export const baseURL = 'https://api-backtube.herokuapp.com'
 export const streamingURL = 'https://api-backtube-stream.herokuapp.com'
 export const ytURL = 'https://chaos87.pythonanywhere.com'
-export const baseURL = 'http://localhost:5000'
+// export const baseURL = 'http://localhost:5000'
 // export const streamingURL = 'http://localhost:5000'
