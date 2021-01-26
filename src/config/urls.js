463e07b0c19d8460d86b131090483d5fc4655bc9
Jonathan Barone <johnito.barone@gmail.com>
2021-01-26 09:45:36 +0800

    using another stream url

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,7 @@
 export const CALLBACK_PATH = '/lastfm/callback';
 
 export const baseURL = 'https://api-backtube.herokuapp.com'
-export const streamingURL = 'https://stream-api-cknd2zehxq-uw.a.run.app'
+export const streamingURL = 'https://stream-api-cknd2zehxq-uk.a.run.app'
 export const ytURL = 'https://chaos87.pythonanywhere.com'
 // export const baseURL = 'http://localhost:5000'
 // export const streamingURL = 'http://localhost:5000'
