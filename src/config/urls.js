61b9a00def046ec8b1a051918818292740c7e715
Jonathan Barone <johnito.barone@gmail.com>
2021-01-29 23:04:36 +0800

    changed stream url

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,7 +1,9 @@
 export const CALLBACK_PATH = '/lastfm/callback';
 
 export const baseURL = 'https://api-backtube.herokuapp.com'
-export const streamingURL = 'https://backtube-stream-api-msxgd.ondigitalocean.app'
+// export const streamingURL = 'https://backtube-stream-api-msxgd.ondigitalocean.app'
+export const streamingURL = 'https://agile-basin-09060.herokuapp.com'
+
 export const ytURL = 'https://chaos87.pythonanywhere.com'
 export const bandcampURL = 'https://api-backtube-bandcamp.herokuapp.com'
 // export const baseURL = 'http://localhost:5000'
