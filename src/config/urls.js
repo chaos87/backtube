139d7f725b0fbd4e9bbe27d2d4e9eddbe2f66fa7
Jonathan Barone <johnito.barone@gmail.com>
2021-03-03 16:22:30 +0800

    removed DO url

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,7 +2,7 @@ export const CALLBACK_PATH = '/lastfm/callback';
 
 export const baseURL = 'https://api-backtube.herokuapp.com'
 // export const streamingURL = 'https://backtube-stream-api-msxgd.ondigitalocean.app'
-export const streamingURL = new Date().getDate() % 2 === 0 ? 'https://backtube-stream-api-msxgd.ondigitalocean.app' : 'https://agile-basin-09060.herokuapp.com'
+export const streamingURL = 'https://agile-basin-09060.herokuapp.com'
 
 export const ytURL = 'https://chaos87.pythonanywhere.com'
 export const bandcampURL = 'https://api-backtube-bandcamp.herokuapp.com'
